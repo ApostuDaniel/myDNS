54b887eaaeb6666d3b4d674ee17b5aff2bea1bed
ApostuDaniel <79244856+ApostuDaniel@users.noreply.github.com>
2022-01-02 21:53:21 +0200

    data commit

---
+++ config/sqlscripts/facebook_com.sql
@@ -0,0 +1,4 @@
+INSERT INTO RR 
+VALUES
+("www", 1000, 1, 1, "69.63.176.13", 12),
+("marketplace.facebook.com.", 1000, 1, 2, "127.0.0.1:2708", 14);
---
+++ config/sqlscripts/nature_leaf.sql
@@ -0,0 +1,4 @@
+INSERT INTO RR 
+VALUES
+("doc", 1000, 1, 1, "3.3.4.4", 7),
+("documentary", 1000, 1, 1, "1.1.2.2", 7);
